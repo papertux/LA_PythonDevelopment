2c057e731227bb37774b939ea8be15167ec36938
David Mansfield <dmansfield@onica.com>
2019-09-06 16:13:35 -0700

    Adding more files

--- learning_python/using_files.py
+++ learning_python/using_files.py
@@ -1,15 +1,13 @@
-with my_file = open('xmen.txt', 'w+') as my_file:
-    my_file.write('Beast\n')
-    my_file.write('Phoenix\n')
-    my_file.writelines([
-        'Cyclops\n',
-        'Bishop\n',
-        'Nightcrawler\n',
+my_file = open('xmen.txt', 'w+')
+my_file.write('Beast\n')
+my_file.write('Pheonix\n')
+my_file.writelines([
+    'Cyclops\n',
+    'Bishop\n',
+    'Nightcrawler\n',
 ])
+my_file.close()
 
-my_file = open('xmen.txt', 'rt')
-with my_file:
-    print(my_file.read())
-    print("I'm reading again")
-    print(my_file.read())
-
+my_file = open('xmen.txt', 'r')
+print(my_file.read())
+my_file.close()
